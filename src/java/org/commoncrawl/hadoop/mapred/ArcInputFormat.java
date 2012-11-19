78232ff183344ae09765ca769dec2cd4f74540d2
evert <evert.lammerts@sara.nl>
2012-11-19 19:13:41 +0100

    Fixed imports

--- src/java/org/commoncrawl/hadoop/mapred/ArcInputFormat.java
+++ src/java/org/commoncrawl/hadoop/mapred/ArcInputFormat.java
@@ -2,8 +2,6 @@ package org.commoncrawl.hadoop.mapred;
 
 import java.io.IOException;
 
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.JobContext;
